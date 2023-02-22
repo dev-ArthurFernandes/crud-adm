dcd42f08559bfdb33b9c3fb043ec76e344f2189e
Arthur de Sousa Fernandes <arthurdesousafernandes@gmail.com>
2023-02-22 08:56:12 -0300

    fix: Put Router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -21,7 +21,7 @@ userRouter.get("", ensureToken, listUsersController)
 userRouter.get("/:id", ensureToken, validateUserId, listUserController)
 userRouter.post("", validatePostEntries, checkPostEntries, validateEmail, createUserController)
 userRouter.patch("/:id", ensureToken,validateUserId, validateEntries, validateEmail, updateUserController)
-userRouter.put('/:id', ensureToken, validateUserId, validateEntries, checkPostEntries, validateEmail, updateUserController)
+userRouter.put('/:id/recover', ensureToken, validateUserId, validateEntries, checkPostEntries, validateEmail, updateUserController)
 userRouter.delete('/:id', ensureToken, validateUserId, deleteUserController)
 
 
