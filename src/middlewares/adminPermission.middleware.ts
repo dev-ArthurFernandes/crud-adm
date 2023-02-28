cd261bc30bc6bccd8909a5645b1b7c532788437e
Arthur <arthurdesosuafernandes@gmail.com>
2023-02-27 21:56:33 -0300

    fix: Hot Fix

--- src/middlewares/adminPermission.middleware.ts
+++ src/middlewares/adminPermission.middleware.ts
@@ -5,15 +5,15 @@ import 'dotenv/config';
 
 const validateAdminPermission = (req: Request, res: Response, next: NextFunction): void => {
 
-    if(parseInt(req.params?.id) === req.user.id && !req.user.admin){
+    if(parseInt(req.params?.id) === req.user.id && !req.user.admin) {
         return next()
     }
 
-    if(parseInt(req.params?.id) === req.user.id && req.user.admin){
+    if(parseInt(req.params?.id) === req.user.id && req.user.admin) {
         return next()
     }
 
-    if(parseInt(req.params?.id) !== req.user.id && !req.user.admin){
+    if(parseInt(req.params?.id) !== req.user.id && !req.user.admin) {
         throw new AppError("Insufficient Permission", 403)
     }
 
@@ -24,6 +24,7 @@ const validateAdminPermission = (req: Request, res: Response, next: NextFunction
     if(req.user.admin){
         return next()
     }
+
 }
 
 export default validateAdminPermission
